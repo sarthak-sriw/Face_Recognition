6b7a359196e7397ab48e95f892b0467c20f0bec8
Sarthak Srivastava <sarthak.btech.ec17@iiitranchi.ac.in>
2020-04-09 21:35:14 +0530

    Add files via upload

---
+++ train_model.py
@@ -0,0 +1,30 @@
+from sklearn.preprocessing import LabelEncoder
+from sklearn.svm import SVC
+import argparse
+import pickle
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-e", "--embeddings", required=True,
+	help="path to serialized db of facial embeddings")
+ap.add_argument("-r", "--recognizer", required=True,
+	help="path to output model trained to recognize faces")
+ap.add_argument("-l", "--le", required=True,
+	help="path to output label encoder")
+args = vars(ap.parse_args())
+
+data = pickle.loads(open(args["embeddings"], "rb").read())
+
+le = LabelEncoder()
+labels = le.fit_transform(data["names"])
+print(labels.shape)
+
+recognizer = SVC(C=1.0, kernel="linear", probability=True)
+recognizer.fit(data["embeddings"], labels)
+
+f = open(args["recognizer"], "wb")
+f.write(pickle.dumps(recognizer))
+f.close()
+
+f = open(args["le"], "wb")
+f.write(pickle.dumps(le))
+f.close()
